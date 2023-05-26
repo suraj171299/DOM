8054d94f0f3facd8ea3642829ea412626f73e9e4
suraj171299 <surajpadalkar1999@gmail.com>
2023-05-26 10:44:26 +0530

    Js Crash COurse

--- dom.js
+++ dom.js
@@ -86,6 +86,6 @@ var odd = document.querySelectorAll('li:nth-child(odd)')
 var even = document.querySelectorAll('li:nth-child(even)')
 
 for(var i =0;i<odd.length;i++){
-    odd[i].style.backgroundColor = '#f4f4f4'
+    od[i].style.backgroundColor = '#f4f4f4'
     even[i].style.backgroundColor = '#ccc'
 }
