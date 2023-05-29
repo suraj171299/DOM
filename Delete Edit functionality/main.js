3e1090aa07fdb57537b58c68070a5235a96de7b7
suraj171299 <surajpadalkar1999@gmail.com>
2023-05-29 10:21:43 +0530

    NO change

--- Delete Edit functionality/main.js
+++ Delete Edit functionality/main.js
@@ -36,6 +36,12 @@ function addItem(e){
 
     // Append li to list
     itemList.appendChild(li)
+
+    // Description form
+    var des = document.getElementById('item2').value
+
+    // Add description to li
+    li.append(document.createTextNode(des))
 }
 
 function removeItem(e){
@@ -53,12 +59,13 @@ function removeItem(e){
 function filterItems(e){
     //convert text to lowercase
     var text = e.target.value.toLowerCase()
-    // Get Lisr
+    // Get List
     var items = itemList.getElementsByTagName('li')
     // Convert to an array
     Array.from(items).forEach(function(item){
         var itemName = item.firstChild.textContent
-        if(itemName.toLowerCase().indexOf(text) != -1){
+        var description = item.childNodes[1].textContent
+        if(itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) != -1){
             item.style.display = 'block'
         }else{
             item.style.display = 'none'
